d3e4f64cfc36409acf758b62911fe0d85ce7b237
Prafful Mali <prafful@gkmit.co>
2025-11-14 16:18:07 +0530

    feat: add user API endpoint

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
-from .views import LoginAPIView, RegisterAPIView, VerifyEmailView, login_view, register_view, dashboard_view, LogoutAPIView, CustomTokenView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import LoginAPIView, RegisterAPIView, VerifyEmailView, login_view, register_view, dashboard_view, UserViewSet, LogoutAPIView, CustomTokenView
 
 app_name = "users"
 
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path("login/", login_view, name="login"),
@@ -13,5 +16,7 @@ urlpatterns = [
     path("api/login/", LoginAPIView.as_view(), name="api_login"),
     path("api/register/", RegisterAPIView.as_view(), name="api_register"),
     path("api/logout/", LogoutAPIView.as_view(), name="api_logout"),
-    path("api/token", CustomTokenView.as_view(), name="api_token"),    
+    path("api/token", CustomTokenView.as_view(), name="api_token"),
+    path("api/", include(router.urls)),
+
 ]
